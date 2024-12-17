e1f79e9b9dd08fb6074b6963f9413c1e9505b80e
Jeremy Keeler <jeremy@keeler.dev>
2024-12-17 13:50:44 -0500

    Switch to nvim tree and fzf-lua for performance

--- configs/.config/nvim/lua/config/remap.lua
+++ configs/.config/nvim/lua/config/remap.lua
@@ -89,28 +89,35 @@ map("n", "<Leader>bd", "<cmd>bdelete<cr>")
 -- DEPRECATED, using fzf-lua instead for performance
 -- " Telescope remaps
 -- """"""""""""""""""""""""""""""
-map("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-map("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find files in buffers" })
-map("n", "<leader>fi", function()
-	require("telescope.builtin").find_files({ no_ignore = true, hidden = true })
-end, { desc = "Find files (including ignored)" })
-map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-map("n", "<leader>fb", "<cmd>Telescope file_browser<cr>")
+-- map("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+-- map("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find files in buffers" })
+-- map("n", "<leader>fi", function()
+-- 	require("telescope.builtin").find_files({ no_ignore = true, hidden = true })
+-- end, { desc = "Find files (including ignored)" })
+-- map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+-- map("n", "<leader>fb", "<cmd>Telescope file_browser<cr>")
 
 -- """"""""""""""""""""""""""""""
 -- " FzfLua remaps
 -- """"""""""""""""""""""""""""""
--- map("n", "<leader>f", "<cmd>FzfLua files<cr>")
--- map("n", "<leader>b", "<cmd>FzfLua buffers<cr>")
--- map("n", "<leader>g", "<cmd>FzfLua live_grep<cr>")
--- map("n", "<leader>fg", "<cmd>FzfLua git_files<cr>")
+map("n", "<leader>f", "<cmd>FzfLua files<cr>")
+map("n", "<leader>b", "<cmd>FzfLua buffers<cr>")
+map("n", "<leader>g", "<cmd>FzfLua live_grep<cr>")
+map("n", "<leader>fg", "<cmd>FzfLua git_files<cr>")
 
 -- """"""""""""""""""""""""""""""
--- " Neotree remaps
+-- " (DEPRECATED) Neotree remaps
 -- """"""""""""""""""""""""""""""
-map("n", "<leader>n", "<cmd>Neotree<cr>")
-map("n", "<leader>nc", "<cmd>Neotree position=current<cr>")
-map("n", "<leader>ng", "<cmd>Neotree float git_status<cr>")
+-- map("n", "<leader>n", "<cmd>Neotree<cr>")
+-- map("n", "<leader>nc", "<cmd>Neotree position=current<cr>")
+-- map("n", "<leader>ng", "<cmd>Neotree float git_status<cr>")
+
+-- """"""""""""""""""""""""""""""
+-- " Nvim-tree remaps
+-- """"""""""""""""""""""""""""""
+map("n", "<leader>n", "<cmd>NvimTreeToggle<cr>")
+-- map("n", "<leader>nc", "<cmd>Neotree position=current<cr>")
+-- map("n", "<leader>ng", "<cmd>Neotree float git_status<cr>")
 
 -- """"""""""""""""""""""""""""""
 -- " Undotree remaps

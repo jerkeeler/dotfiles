7a037f5428ce43b671527c1bc105b8ba9936980d
Jeremy Keeler <jeremy@keeler.dev>
2024-12-18 11:09:54 -0500

    Fix merge conflict

--- configs/.config/nvim/lua/plugins/formatter.lua
+++ configs/.config/nvim/lua/plugins/formatter.lua
@@ -21,7 +21,7 @@ return {
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 2500,
 			},
 		})
 
@@ -29,7 +29,7 @@ return {
 			conform.format({
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 2500,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,

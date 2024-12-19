9085d92294ed80b8b506446bb8132422d58313b7
Jeremy Keeler <jeremy@keeler.dev>
2024-12-19 09:47:56 -0500

    Add Dracula fish theme

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -2,5 +2,6 @@ fish_add_path /opt/homebrew/bin/
 starship init fish | source
 zoxide init fish | source
 
+fish_config theme choose "Dracula Official"
 set -Ux EDITOR "nvim"
 set -Ux LSCOLORS "gxfxbEaEBxxEhEhBaDaCaD"

75728a6e49f57c11ce9f60d92cb379318021b801
Jeremy Keeler <jeremy@keeler.dev>
2026-01-25 21:21:53 -0500

    Add back format on save

--- configs/.config/nvim/lua/plugins/lspconfig.lua
+++ configs/.config/nvim/lua/plugins/lspconfig.lua
@@ -10,6 +10,22 @@ return {
 	config = function()
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+		-- Format on save when LSP supports it
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if client and client.server_capabilities.documentFormattingProvider then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = vim.api.nvim_create_augroup("Format", { clear = true }),
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
+		})
+
 		-- Default config for all servers
 		vim.lsp.config("*", {
 			capabilities = capabilities,
